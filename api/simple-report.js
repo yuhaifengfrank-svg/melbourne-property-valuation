4be751ab4844ae81980d13817d311250630dec1d
Haifeng Yu <yuhaifengfrank@gmail.com>
2026-06-17 15:08:56 +1000

    fix: simple-report pass fetch:false to runValuation

--- api/simple-report.js
+++ api/simple-report.js
@@ -41,7 +41,9 @@ export default async function handler(req, res) {
       suburb: body.suburb || suburb,
       state,
       propertyType: propertyType.toLowerCase(),
-      landSize: body.landSize || null,
+      landSize: body.landSize || null
+    }, {
+      fetch: false,
       useDatabaseFallback: true
     });
 
